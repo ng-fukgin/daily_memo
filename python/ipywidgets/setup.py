0ca6001240146e7889f42d7fb3df184da69dbaa3
fujianwu <46244590+txwfj@users.noreply.github.com>
2023-04-17 09:35:33 +0800

    update

--- python/ipywidgets/setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-# Copyright (c) IPython Development Team.
-# Copyright (c) Jupyter Development Team.
-# Distributed under the terms of the Modified BSD License.
-
-from setuptools import setup
-if __name__ == '__main__':
-    setup()
